ebdd43b0eaa1a8a803d145cc039bf44ed79b1d6c
Shiay6Lan <77952669+Shiay6Lan@users.noreply.github.com>
2021-01-26 12:14:22 +0000

    Update all.h

--- all.h
+++ all.h
@@ -43,9 +43,54 @@ struct NODE* creatnode(struct student data)        //创建结点
 	}
 	return node;
 }
-void insertnode(struct NODE* headnode, struct student data)       //插入节点
+void printnode(struct NODE* node)               //打印当前结点
+{
+	printf("\t\t\t    学号\t姓名   学院代码  性别代码  C语言成绩  数学成绩  英语成绩  体育成绩\n");
+	printf("\t\t\t%s   %s   %s\t    %c\t      %d\t %d\t   %d\t     %d\n", node->data.num, node->data.name, node->data.instnum, node->data.sex, node->data.score.cscore, node->data.score.mscore, node->data.score.escore, node->data.score.pscore);
+}
+void insertnode(struct NODE* headnode, struct student data)       //插入结点
 {
 	struct NODE* node = creatnode(data);
 	node->next = headnode->next;
 	headnode->next = node;
+}
+struct NODE* searchnode(struct NODE* headnode, char* name)      //按名字查找结点
+{
+	struct NODE* pmove = headnode->next;
+	if (pmove == NULL) return pmove;
+	else
+	{
+		while (strcmp(pmove->data.name, name))
+		{
+			pmove = pmove->next;
+			if (pmove == NULL) break;
+		}
+		return pmove;
+	}
+}
+void deletenode(struct NODE* headnode, char* num)             //删除结点
+{
+	struct NODE* frontnode = headnode;
+	struct NODE* posnode = headnode->next;
+	if (posnode == NULL)
+	{
+		printf("\t\t\t未查找到相关学生信息！\n");
+		return;
+	}
+	else
+	{
+		while (strcmp(posnode->data.num, num))
+		{
+			frontnode = posnode;
+			posnode = frontnode->next;
+			if (posnode == NULL)
+			{
+				printf("\t\t\t未查找到相关学生信息！\n");
+				return;
+			}
+		}
+		frontnode->next = posnode->next;
+		free(posnode);
+		printf("\t\t\t删除成功！\n");
+	}
 }
