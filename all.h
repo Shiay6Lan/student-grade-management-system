072a0ed1af83041ddca88b321820c0fe3246d254
Shiay6Lan <77952669+Shiay6Lan@users.noreply.github.com>
2021-01-26 06:07:55 +0000

    Update all.h

--- all.h
+++ all.h
@@ -2,19 +2,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-struct inst
+struct inst                       //学院
 {
 	char instnum[20];
 	char instname[20];
 };
-struct SCORE
+struct SCORE                      //分数
 {
 	int cscore;
 	int mscore;
 	int escore;
 	int pscore;
 };
-struct student
+struct student                     //学生信息
 {
 	char num[20];
 	char name[20];
@@ -22,18 +22,18 @@ struct student
 	char sex;
 	struct SCORE score;
 };
-struct NODE
+struct NODE                        //链表结点
 {
 	struct student data;
 	struct NODE* next;
 };
-struct NODE* creatlist()
+struct NODE* creatlist()           //创建链表
 {
 	struct NODE* listhead = (struct NODE*)malloc(sizeof(struct NODE));
 	if (listhead != NULL) listhead->next = NULL;
 	return listhead;
 }
-struct NODE* creatnode(struct student data)
+struct NODE* creatnode(struct student data)        //创建结点
 {
 	struct NODE* node = (struct NODE*)malloc(sizeof(struct NODE));
 	if (node != NULL)
@@ -43,7 +43,7 @@ struct NODE* creatnode(struct student data)
 	}
 	return node;
 }
-void insertnode(struct NODE* headnode, struct student data)
+void insertnode(struct NODE* headnode, struct student data)       //插入节点
 {
 	struct NODE* node = creatnode(data);
 	node->next = headnode->next;
