432a451dfe899d4c1eda961fdd2dc35c32f9f367
Shiay6Lan <77952669+Shiay6Lan@users.noreply.github.com>
2021-01-27 06:26:11 +0000

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ void menu()
 }
 int key()
 {
-	int down;
+	int down, down2;
 	struct student tempdata;
 	scanf("%d", &down);
 	switch (down)
@@ -40,22 +40,52 @@ int key()
 			printf("请输入学生学号、姓名、学院代码、性别代码(M为男，F为女)、C语言成绩、数学成绩、英语成绩、体育成绩:\n");
 			scanf("%s%s%s%s%d%d%d%d", tempdata.num, tempdata.name, tempdata.instnum, &tempdata.sex, &tempdata.score.cscore, &tempdata.score.mscore, &tempdata.score.escore, &tempdata.score.pscore);
 			insertnode(listhead, tempdata);
+			printf("\t\t\t添加成功！\n");
+			savefile("Stu_Info.txt", listhead);
 			break;
 		case 5:
 			printf("\t\t\t\t\t      【  修改学生成绩信息  】\n");
+			printf("\t\t\t请输入要修改学生信息的学生学号:");
+			scanf("%s", tempdata.num);
+			if (searchnodebynum(listhead, tempdata.num) == NULL) printf("\t\t\t未查找到相关学生信息！\n");
+			else
+			{
+				struct NODE* node = searchnodebynum(listhead, tempdata.num);
+				printf("\t\t\t请输入要修改的学生信息(0-返回菜单,1-学生所有信息,2-学生成绩信息):");
+				scanf("%d", &down2);
+				switch (down2)
+				{
+				case 0:
+					break;
+				case 1:
+					printf("\t\t\t请输入新的学生信息(学号、姓名、学院代码、性别代码(M为男，F为女)、C语言成绩、数学成绩、英语成绩、");
+					printf("\t\t\t\t体育成绩):");
+					scanf("%s%s%s%s%d%d%d%d", node->data.num, node->data.name, node->data.instnum, &node->data.sex, &node->data.score.cscore, &node->data.score.mscore, &node->data.score.escore, &node->data.score.pscore);
+					printf("\t\t\t修改成功！\n");
+					savefile("Stu_Info.txt", listhead);
+					break;
+				case 2:
+					printf("\t\t\t请输入新的学生成绩信息(C语言成绩、数学成绩、英语成绩、体育成绩):");
+					scanf("%d%d%d%d", &node->data.score.cscore, &node->data.score.mscore, &node->data.score.escore, &node->data.score.pscore);
+					printf("\t\t\t修改成功！\n");
+					savefile("Stu_Info.txt", listhead);
+					break;
+				}
+			}
 			break;
 		case 6:
 			printf("\t\t\t\t\t      【   按姓名查找学生   】\n");
 			printf("\t\t\t请输入学生姓名:");
 			scanf("%s", tempdata.name);
-			if (searchnode(listhead, tempdata.name) == NULL) printf("\t\t\t未查找到相关学生信息！\n");
-			else printnode(searchnode(listhead, tempdata.name));
+			if (searchnodebyname(listhead, tempdata.name) == NULL) printf("\t\t\t未查找到相关学生信息！\n");
+			else printnode(searchnodebyname(listhead, tempdata.name));
 			break;
 		case 7:
 			printf("\t\t\t\t\t      【    删除学生信息    】\n");
 			printf("\t\t\t请输入要删除学生信息的学号:");
 			scanf("%s", tempdata.num);
 			deletenode(listhead, tempdata.num);
+			savefile("Stu_Info.txt", listhead);
 			break;
 		case 8:
 			printf("\t\t\t\t\t      【    修改系统密码    】\n");
@@ -69,6 +99,7 @@ int main()
 {
 	listhead = creatlist();
 
+	readfile("Stu_Info.txt", listhead);
 	while (1)
 	{
 		menu();
